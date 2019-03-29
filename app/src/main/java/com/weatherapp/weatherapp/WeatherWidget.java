aac02b5ede5142bb4df12e520503dd34bc650042
kosjak95 <kosmikos@op.pl>
2019-03-29 21:27:45 +0100

    widget on click

--- app/src/main/java/com/weatherapp/weatherapp/WeatherWidget.java
+++ app/src/main/java/com/weatherapp/weatherapp/WeatherWidget.java
@@ -1,48 +1,64 @@
 package com.weatherapp.weatherapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
 import io.paperdb.Paper;
 
 public class WeatherWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
 
-        Paper.init(context);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
-        views.setTextViewText(R.id.appwidget_city, (CharSequence) Paper.book().read("City"));
-        views.setTextViewText(R.id.appwidget_temp,Paper.book().read("Temperature") + "°C");
-
-        Resources res = context.getResources();
-        String mDrawableName =  Paper.book().read("Icon");
-        int resID = res.getIdentifier(mDrawableName , "drawable", context.getPackageName());
-        views.setImageViewResource(R.id.appwidget_icon, resID);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+    private static final String WidgetLayout = "widgetLayout";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        Paper.init(context);
+
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context, WeatherWidget.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+        for (int widgetId : allWidgetIds) {
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
+
+            remoteViews.setTextViewText(R.id.appwidget_city, (CharSequence) Paper.book().read("City"));
+            remoteViews.setTextViewText(R.id.appwidget_temp,Paper.book().read("Temperature") + "°C");
+            Resources res = context.getResources();
+            String mDrawableName =  Paper.book().read("Icon");
+            int resID = res.getIdentifier(mDrawableName , "drawable", context.getPackageName());
+            remoteViews.setImageViewResource(R.id.appwidget_icon, resID);
+
+            remoteViews.setOnClickPendingIntent(R.id.widgetLayout, getPendingSelfIntent(context, WidgetLayout));
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 
     @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
+    public void onReceive(Context context, Intent intent) {
+
+        super.onReceive(context, intent);//add this line
+        if (WidgetLayout.equals(intent.getAction())) {
+            Intent myIntent = new Intent(context, MainActivity.class);
+            context.startActivity(myIntent);
+        }
+    };
 }
 
